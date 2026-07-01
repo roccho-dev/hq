6725430afd18232d2023b38ca67b3bd815b3c53f
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-01 15:20:49 +0900

    Add semantic suggestion model (#2)
    * Expose semantic suggestion model
    
    * Add semantic suggestion model
    
    * Add semantic suggestion tests
    
    * Document semantic suggestion work
    
    * Document stack ready state

--- hq/__init__.py
+++ hq/__init__.py
@@ -1,7 +1,10 @@
 from .core import CursorContext, JsonlWorld, derive_cursor_context
+from .suggestion import Suggestion, suggest_keys
 
 __all__ = [
     "CursorContext",
     "JsonlWorld",
+    "Suggestion",
     "derive_cursor_context",
+    "suggest_keys",
 ]
