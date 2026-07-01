db731233f11e382eb60aba016e6900d7bffe2ab6
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-01 18:03:18 +0900

    Export engine suggestion helpers

--- hq/__init__.py
+++ hq/__init__.py
@@ -1,10 +1,14 @@
 from .core import CursorContext, JsonlWorld, derive_cursor_context
-from .suggestion import Suggestion, suggest_keys
+from .suggestion import Diagnostic, Suggestion, diagnose_keys, suggest_all, suggest_keys, suggest_values
 
 __all__ = [
     "CursorContext",
+    "Diagnostic",
     "JsonlWorld",
     "Suggestion",
     "derive_cursor_context",
+    "diagnose_keys",
+    "suggest_all",
     "suggest_keys",
+    "suggest_values",
 ]
