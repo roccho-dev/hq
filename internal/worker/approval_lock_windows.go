f8ef4f70b2b401527b3e33455afd26bf8e33be19
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-15 19:10:44 +0900

    Use kernel32 approval file locking

--- internal/worker/approval_lock_windows.go
+++ internal/worker/approval_lock_windows.go
@@ -4,16 +4,42 @@ package worker
 
 import (
 	"os"
+	"syscall"
+	"unsafe"
+)
 
-	"golang.org/x/sys/windows"
+const lockFileExclusiveLock = 0x2
+
+var (
+	lockFileExW   = syscall.NewLazyDLL("kernel32.dll").NewProc("LockFileEx")
+	unlockFileExW = syscall.NewLazyDLL("kernel32.dll").NewProc("UnlockFileEx")
 )
 
 func lockWorkspaceApprovalFile(file *os.File) error {
-	var overlapped windows.Overlapped
-	return windows.LockFileEx(windows.Handle(file.Fd()), windows.LOCKFILE_EXCLUSIVE_LOCK, 0, 1, 0, &overlapped)
+	var overlapped syscall.Overlapped
+	result, _, callErr := lockFileExW.Call(
+		file.Fd(), uintptr(lockFileExclusiveLock), 0, 1, 0,
+		uintptr(unsafe.Pointer(&overlapped)),
+	)
+	if result != 0 {
+		return nil
+	}
+	if callErr != nil && callErr != syscall.Errno(0) {
+		return callErr
+	}
+	return syscall.EINVAL
 }
 
 func unlockWorkspaceApprovalFile(file *os.File) error {
-	var overlapped windows.Overlapped
-	return windows.UnlockFileEx(windows.Handle(file.Fd()), 0, 1, 0, &overlapped)
+	var overlapped syscall.Overlapped
+	result, _, callErr := unlockFileExW.Call(
+		file.Fd(), 0, 1, 0, uintptr(unsafe.Pointer(&overlapped)),
+	)
+	if result != 0 {
+		return nil
+	}
+	if callErr != nil && callErr != syscall.Errno(0) {
+		return callErr
+	}
+	return syscall.EINVAL
 }
